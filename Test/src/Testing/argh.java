7c6d1739556e101b511c1fc530562c7e4fcc4bc5
bharg <bharg@DESKTOP-9JHJ27R>
2021-05-05 17:20:54 +0530

    Initiated first code

---
+++ Test/src/Testing/argh.java
@@ -0,0 +1,26 @@
+package Testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+public class argh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.close();
+		driver.quit();
+		
+		
+		
+		
+
+	}
+
+}
